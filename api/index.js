d80aa939fb7dc940735120230812d44fb9043d1b
Bhandoth Pavan <pavannyk99@gmail.com>
2024-10-02 19:56:03 +0530

    #blop-post

--- api/index.js
+++ api/index.js
@@ -28,9 +28,9 @@ app.use("/api", userRoute);
 app.use("/api", authRoute);
 app.use("/api", postRoute);
 app.use("/api/comment",commentRoute)
-app.use(express.static(path.join(__dirname , 'client/dist')))
+app.use(express.static(path.join(__dirname , '../client/dist')))
 app.use('*',(req , res)=>{
-  res.sendFile(path.join(__dirname ,"client" ,"dist" , "index.html"))
+  res.sendFile(path.join(__dirname ,'../client/dist/index.html'))
 } )
 app.listen(PORT, () => {
   console.log(`Server Started At :${PORT}`);
